1d2414eaaea09c44977b9e8e211601b588fe3771
telun <teddy.lun@agoda.com>
2020-07-29 23:31:27 +0700

    102

---
+++ leetCode/102/102.py
@@ -0,0 +1,26 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: TreeNode) -> List[List[int]]:
+        levels = []
+        if not root:
+            return levels
+
+        def helper(node, level):
+            if len(levels) == level:
+                levels.append([])
+
+            levels[level].append(node.val)
+
+            if node.left is not None:
+                helper(node.left, level + 1)
+
+            if node.right is not None:
+                helper(node.right, level + 1)
+
+        helper(root, 0)
+        return levels
